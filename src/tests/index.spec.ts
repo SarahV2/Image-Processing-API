99b343e2a6d4eea4e472180c311c5d6e8c03295a
Sarah <qsalahmadi@gmail.com>
2022-09-25 23:21:57 +0300

    add: first test suite skeleton

--- src/tests/index.spec.ts
+++ src/tests/index.spec.ts
@@ -1,5 +1,12 @@
+describe("checkForExistingImage()", () => {
+  it("should return True if an image with the provided filename exists", () => {});
+  it("should return False if an image with the provided filename does not exist", () => {});
+});
 
-it('should run the test',()=>{
-    console.log('Test started successfully!')
-    expect(1+1).toEqual(2)
-})
+describe("processImage()", () => {
+  it("should return existing thumbnail file path if it already exists", () => {});
+  it(
+    "should create and return the new processed thumbnail path if it does not exist "
+  );
+  it("should throw an error if the source/original image does not exist", () => {});
+});
